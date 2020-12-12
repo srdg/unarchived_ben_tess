02318ee963223da4b2ef09abf0b9e06152e0b943
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-12-12 12:30:38 +0530

    Update line_hocr.sh

--- line_hocr.sh
+++ line_hocr.sh
@@ -5,8 +5,8 @@ set -- "$SOURCE"*.png
 for img_file; do
     echo -e  "\r\n File: $img_file"
     OMP_THREAD_LIMIT=1 tesseract  "${img_file}" "${img_file%.*}"  --psm 6  --oem 1  -l $lang ben page_separator='' hocr
-    PYTHONIOENCODING=UTF-8 ./hocr-extract-images -b ./myfiles/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
+    PYTHONIOENCODING=UTF-8 ./hocr-extract-images -b ./010_LetterPress-Data/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
 done
-rename s/exp0.txt/exp0.gt.txt/ ./myfiles/*exp0.txt
+#rename s/exp0.txt/exp0.gt.txt/ ./myfiles/*exp0.txt
 
 echo "Image files converted to tif. Correct the ground truth files and then run ocr-d train to create box and lstmf files"
