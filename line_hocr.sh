3fc9686ac6c1f2b504995ee8b4cde23122d3281a
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-12-12 12:51:34 +0530

    Update line_hocr.sh

--- line_hocr.sh
+++ line_hocr.sh
@@ -5,7 +5,7 @@ set -- "$SOURCE"*.png
 for img_file; do
     echo -e  "\r\n File: $img_file"
     OMP_THREAD_LIMIT=1 tesseract  "${img_file}" "${img_file%.*}"  --psm 6  --oem 1  -l $lang ben page_separator='' hocr
-    PYTHONIOENCODING=UTF-8 ./hocr-extract-images -b ./010_LetterPress-Data/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
+    PYTHONIOENCODING=UTF-8 ./hocr-tools-master/hocr-extract-images -b ./010_LetterPress-Data/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
 done
 #rename s/exp0.txt/exp0.gt.txt/ ./myfiles/*exp0.txt
 
