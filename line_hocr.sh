55785714bf80965be305f1cbe1a6e2197fc9327a
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-12-12 13:00:44 +0530

    Update line_hocr.sh

--- line_hocr.sh
+++ line_hocr.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 SOURCE="./010_LetterPress-Data/"
 lang=ben
-rm -rf ./tesstrain-master/data/ben-ground-truth && mkdir -p ./tesstrain-master/data/ben-ground-truth
+rm -rf ./data/ben-ground-truth && mkdir -p ./data/ben-ground-truth
 set -- "$SOURCE"*.png
 for img_file; do
     echo -e  "\r\n File: $img_file"
     OMP_THREAD_LIMIT=1 tesseract  "${img_file}" "${img_file%.*}"  --psm 6  --oem 1  -l $lang ben page_separator='' hocr
-    PYTHONIOENCODING=UTF-8 ./hocr-tools-master/hocr-extract-images -b ./010_LetterPress-Data/ -p ./tesstrain-master/data/ben-ground-truth/"${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
+    PYTHONIOENCODING=UTF-8 ./hocr-tools-master/hocr-extract-images -b ./010_LetterPress-Data/ -p "${img_file%.*}"-%03d.exp0.tif  "${img_file%.*}".hocr 
 done
 #rename s/exp0.txt/exp0.gt.txt/ ./myfiles/*exp0.txt
-
+!mv ./010_LetterPress-Data/*.tif ./data/ben-ground-truth/
+!mv ./010_LetterPress-Data/*exp0.txt ./data/ben-ground-truth/
 echo "Image files converted to tif. Correct the ground truth files and then run ocr-d train to create box and lstmf files"
