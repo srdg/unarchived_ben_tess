b87e38c511702ba337687f4be1ec2e34f803ae5d
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-12-18 18:57:39 +0530

    Update e2ep.sh

--- src/e2ep.sh
+++ src/e2ep.sh
@@ -1,15 +1,15 @@
 echo "*******************Install dependencies***************************"
-apt install tesseract-ocr bc libtesseract-dev 
+sudo apt-get install tesseract-ocr bc libtesseract-dev 
 echo "********************Verify installation**************************"
 which tesseract
 echo "*******************Install language***************************"
-apt install tesseract-ocr-ben
+sudo apt-get install tesseract-ocr-ben
 echo "*********************Verify language*************************"
 ls /usr/share/tesseract-ocr/4.00/tessdata | grep traineddata
 echo "**********************Download training data************************"
 mkdir -p ./data/ben-ground-truth
 wget https://github.com/srdg/unarchived_ben_tess/releases/download/v0.0.2-alpha/data.zip -q --show-progress 
-unzip -q ./data.zip -d ./data/ben-ground-truth && rm -rf data.zip
+unzip -q ./data.zip -d ./data/ben-ground-truth && rm -rf ./data.zip
 echo "***********************Move training  data***********************"
 mv ./data ./tesstrain-master/
 echo "======================= READY TO TRAIN ========================="
